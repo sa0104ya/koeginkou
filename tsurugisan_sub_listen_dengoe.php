090e5c16b344cc06a5a7075f38feaa41180a5dd3
sa0104ya <sa0104ya@gmail.com>
2014-11-14 09:37:55 +0900

    Create tsurugisan_sub_listen_dengoe.php

---
+++ tsurugisan_sub_listen_dengoe.php
@@ -0,0 +1,21 @@
+<?php
+require_once("data/db_info.php");
+$s=mysql_connect($SERV,$USER,$PASS) or die("失敗しました");
+mysql_set_charset("utf8");
+mysql_select_db($DBNM);
+
+$updir = "./";
+$filename = $_FILES['sample']['name'];
+$date = $_POST['date'];
+$username = $_POST['username'];
+ 
+
+
+//テーブルのカウントを取得
+$result = mysql_query("SELECT * FROM tsurugisanrokuon");
+$num_rows = mysql_num_rows($result);
+echo $num_rows;
+
+
+mysql_close($s);
+?>
